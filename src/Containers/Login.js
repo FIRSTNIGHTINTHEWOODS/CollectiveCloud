665038a92ab6ab7031f5e97b4dc429289083b74b
Vyacheslav <van_gogh@konstantah.com>
2018-11-04 21:06:23 +0200

    Update Login.js

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -5,19 +5,15 @@ export default class Login extends Component {
 
 
 
-  createUser() {
-
-       auth.createUserWithEmailAndPassword('dasd@mail.cu', '123123123').then(value => {
-        console.log(value);
-          debugger;
+  createUser(login, password) {
+       auth.createUserWithEmailAndPassword(login, password).then(value => {
+         //success
        })
        .catch(error => {
-             debugger;
-         console.log(error);
+        //
       });
       auth.onAuthStateChanged(firebaseUser => {
         console.log(firebaseUser);
-        debugger;
       });
   }
 
@@ -25,20 +21,19 @@ export default class Login extends Component {
     auth.signInWithEmailAndPassword('ntu347ftit@gmail.com', 'laichzeit1');
     auth.onAuthStateChanged(firebaseUser => {
       console.log(firebaseUser);
-      debugger;
     });
   }
+  
   logOut() {
     auth.signOut();
     auth.onAuthStateChanged(firebaseUser => {
       console.log(firebaseUser);
-      debugger;
     });
   }
 
   render () {
     return  (
-      <div> {this.logOut()} </div>
+      <div onClick={this.logOut}> Exit </div>
     )
    }
 }
